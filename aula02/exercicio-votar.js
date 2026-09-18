668cfc07372d806383d9529e22dee58a3649e373
Alison Diego Cordeiro <alison.diego01@gmail.com>
2026-09-18 16:04:07 -0300

    Exercico votar funcionando

--- aula02/exercicio-votar.js
+++ aula02/exercicio-votar.js
@@ -1,11 +1,10 @@
-let nome = "Alison";
-let idade = 32;
+    let nome = "Alison";
+    let idade = 12;
 
-if (idade >= 16 ||  idade < 18) {
-    console.log(nome + " pode votar");
-} if (idade > 18) {
-    console.log(nome + " é obrigado a votar")
-}  
-else {
-    console.log(nome + " não pode votar");
-}
+    if (idade >= 18) {
+        console.log(nome + " é obrigado a votar");
+    } else if (idade >= 16 && idade <= 17) {
+        console.log(nome + " pode votar");
+    } else {
+        console.log(nome + " não pode votar");
+    }
