fcc63a03514a373a16e657717ff84d61dcb287f7
Alison Diego Cordeiro <alison.diego01@gmail.com>
2026-09-18 19:32:34 -0300

    Correção com !

--- aula02/exercicio-votar.js
+++ aula02/exercicio-votar.js
@@ -2,9 +2,9 @@
     let idade = 13;
 
     if (idade >= 18 && idade < 70) {
-        console.log(nome + " é obrigado a votar");
+        console.log(nome + " é obrigado a votar!");
     } else if (idade >= 16 && idade <= 17) {
-        console.log(nome + " pode votar");
+        console.log(nome + " pode votar mas não é obrigatório!");
     } else {
-        console.log(nome + " não pode votar");
+        console.log(nome + " não pode votar!");
     }
