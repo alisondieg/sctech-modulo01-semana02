005208943f46bd08a0469061dd7bd5e1377d3139
Alison Diego Cordeiro <alison.diego01@gmail.com>
2026-09-17 21:02:43 -0300

    RElacionais

---
+++ aula02/relacionais.js
@@ -0,0 +1,17 @@
+let nome = "João";
+
+if (nome == "Maria") {
+    console.log("nomes igauos");
+} else {
+    console.log("nomes diferentes");
+}
+
+altura = 1.76;
+
+if (altura === 1.76) {
+    console.log("altura 1.76m");
+
+} else {
+    console.log("altura NÂO é 1.76m")
+}
+
