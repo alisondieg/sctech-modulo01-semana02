89d6b93493aec6103241fa31449669814a71dd82
Alison Diego Cordeiro <alison.diego01@gmail.com>
2026-09-18 21:11:03 -0300

    Exercicio switch-case

---
+++ aula03/switch-case.js
@@ -0,0 +1,10 @@
+let diaSemanaNum = 1;
+
+switch (diaSemanaNum) {
+    case 1 :
+        console.log("Hoje é Domingo")
+        break
+
+        case 2 :
+            console.log("Hoje é segunda")
+}
---
+++ aula03/ternarios.js
@@ -0,0 +1,9 @@
+let idade = 20; 
+let maioridade = idade >= 18 ? "Adulto" : "Menor"
+console.log(maioridade)
+
+let mediaAluno = 6;
+let resultado = mediaAluno >= 7 ? "Aprovado" : "Reprovado"
+console.log(resultado)
+
+let parOuImpar = idade % 2
