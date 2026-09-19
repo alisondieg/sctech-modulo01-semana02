8e9361e4c5fdef7950294918f05efea7343cd63b
Alison Diego Cordeiro <alison.diego01@gmail.com>
2026-09-19 00:59:14 -0300

    Exclusao do comentario com as regras do exercicio de switch

--- Nova pasta/exercicio.js
+++ Nova pasta/exercicio.js
@@ -1,18 +1,3 @@
-/*Construa uma calculadora, em que você possui 3 variáveis:
-
-let numero1 = 10;
-
-let numero2 = 5;
-
-let operador = "*";
-
-Usando switch, execute a operação correspondente:
-
-+, -, *, /
-
-O que fazer quando o operador for inválido?
-*/
-
 let numero1 = 10;
 let numero2 = 5;
 let operador = "-";
