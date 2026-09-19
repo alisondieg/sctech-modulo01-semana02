16547388123b79e4f01e58c96cee0a8bbe7fe4cf
Alison Diego Cordeiro <alison.diego01@gmail.com>
2026-09-18 21:43:33 -0300

    Adição do comentario no exercicio

--- aula03/exercicio.js
+++ aula03/exercicio.js
@@ -1,24 +1,39 @@
+/*Construa uma calculadora, em que você possui 3 variáveis:
+
+let numero1 = 10;
+
+let numero2 = 5;
+
+let operador = "*";
+
+Usando switch, execute a operação correspondente:
+
++, -, *, /
+
+O que fazer quando o operador for inválido?
+*/
+
 let numero1 = 10;
 let numero2 = 5;
 let operador = "-";
 
 switch (operador) {
-    case "+":
-        console.log(numero1 + numero2);
-        break;
+  case "+":
+    console.log(numero1 + numero2);
+    break;
 
-    case "-":
-        console.log(numero1 - numero2);
-        break;
+  case "-":
+    console.log(numero1 - numero2);
+    break;
 
-    case "*":
-        console.log(numero1 * numero2);
-        break;
+  case "*":
+    console.log(numero1 * numero2);
+    break;
 
-    case "/":
-        console.log(numero1 / numero2);
-        break;
+  case "/":
+    console.log(numero1 / numero2);
+    break;
 
-    default:
-        console.log("Operador inválido");
+  default:
+    console.log("Operador inválido");
 }
